c5170af445087b178520e677f755e10ce9f42d92
Jose Delgado <josdelgaur@gmail.com>
2023-02-23 18:45:49 -0300

    chore: first commit

---
+++ src/components/BaseButton/BaseButton.tsx
@@ -0,0 +1,54 @@
+import React, { ForwardedRef, forwardRef, memo, ReactNode, useMemo } from 'react';
+import classnames from 'classnames';
+
+import css from './BaseButton.module.scss';
+
+type BaseProps = {
+  className?: string | null;
+  children?: ReactNode;
+  tabIndex?: number | string;
+  disabled?: boolean;
+  'aria-label'?: string;
+  title?: string;
+  onClick?: Function;
+  onBlur?: Function;
+  onFocus?: Function;
+  onKeyDown?: Function;
+  onKeyUp?: Function;
+  onMouseMove?: Function;
+  onMouseEnter?: Function;
+  onMouseLeave?: Function;
+  onMouseUp?: Function;
+  onMouseDown?: Function;
+  onTouchEnd?: Function;
+  onTouchMove?: Function;
+  onTouchStart?: Function;
+};
+
+type FComponent =
+  | string
+  | React.FunctionComponent<
+      BaseProps & {
+        ref: React.Ref<HTMLElement> | null;
+        role: string | null;
+      }
+    >;
+
+export type Props = BaseProps & {
+  component?: FComponent;
+};
+
+const BaseButton = (
+  { className = null, component: Component = 'button', children = null, ...buttonProps }: Props,
+  ref: ForwardedRef<HTMLElement | HTMLButtonElement>
+) => {
+  const currRole = useMemo(() => (Component === 'button' ? null : 'button'), [Component]);
+
+  return (
+    <Component className={classnames(css.BaseButton, className)} ref={ref} role={currRole} {...buttonProps}>
+      {children}
+    </Component>
+  );
+};
+
+export default memo(forwardRef(BaseButton));
