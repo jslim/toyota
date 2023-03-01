ca4d0aed6155a51d6c2832140ca3e3dfb3a36452
Jose Delgado <josdelgaur@gmail.com>
2023-03-01 19:48:51 -0300

    chore: linting

--- src/components/Card/Card.stories.tsx
+++ src/components/Card/Card.stories.tsx
@@ -11,11 +11,13 @@ export const News: Story<CardProps> = (args) => <Card {...args} />;
 
 export const Office: Story<CardProps> = (args) => <Card {...args} />;
 
+const url = 'https://i1.wp.com/thetalkinggeek.com/wp-content/uploads/2015/09/sintel1.png?fit=1920%2C817&ssl=1';
+
 Product.args = {
   title: 'Arene',
   text: 'Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Vestibulum id ligula porta felis euismod semper.',
   image: {
-    src: 'https://i1.wp.com/thetalkinggeek.com/wp-content/uploads/2015/09/sintel1.png?fit=1920%2C817&ssl=1',
+    src: url,
     alt: ''
   },
   cardType: cards.PRODUCT
@@ -26,7 +28,7 @@ News.args = {
   title: 'automated driving',
   text: 'Woven Planet’s CEO James Kuffner presented at Toyota Motor.',
   image: {
-    src: 'https://i1.wp.com/thetalkinggeek.com/wp-content/uploads/2015/09/sintel1.png?fit=1920%2C817&ssl=1',
+    src: url,
     alt: ''
   },
   cardType: cards.NEWS
@@ -35,7 +37,7 @@ Office.args = {
   title: 'Global Headquarters - Tokyo',
   text: 'Nihonbashi Muromachi Mitsui Tower,3-2-1 Nihonbashimuromachi, Chuo-ku,Tokyo, JAPAN 103-0022',
   image: {
-    src: 'https://i1.wp.com/thetalkinggeek.com/wp-content/uploads/2015/09/sintel1.png?fit=1920%2C817&ssl=1',
+    src: url,
     alt: ''
   },
   cardType: cards.OFFICE
