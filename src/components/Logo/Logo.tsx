1eed1995675424bc2f18d3e3385596946b07202d
DariaV13 <daria.vyskubina@jam3.com>
2023-03-01 14:05:10 -0500

    feature: nav + mobileNav

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,24 @@
+import { FC, memo } from 'react';
+import classNames from 'classnames';
+
+import css from './Logo.module.scss';
+import BaseLink from '../BaseLink/BaseLink';
+import LogoSVG from '@/components/svgs/logo.svg';
+
+export type LogoProps = {
+  className?: string;
+  title?: string;
+  href?: string;
+};
+
+const Logo: FC<LogoProps> = ({ className, title, href }) => {
+  return (
+    <div className={classNames('Logo', css.root, className)}>
+      <BaseLink title={title} href={href}>
+        <LogoSVG className={css.logo} />
+      </BaseLink>
+    </div>
+  );
+};
+
+export default memo(Logo);
