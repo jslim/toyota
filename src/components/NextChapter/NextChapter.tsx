921774ab2bebeef71fd54f31a2375f2973136efc
Jose Delgado <josdelgaur@gmail.com>
2023-03-02 13:25:24 -0300

    chore: before rebasing

--- src/components/NextChapter/NextChapter.tsx
+++ src/components/NextChapter/NextChapter.tsx
@@ -1,5 +1,5 @@
 import { FC, memo } from 'react';
-import classNames from 'classnames';
+import classnames from 'classnames';
 
 import css from './NextChapter.module.scss';
 
@@ -9,21 +9,23 @@ import { Props as LinkProps } from '@/components/BaseLink/BaseLink';
 
 export type NextChapterProps = {
   className?: string;
-  // eyebrow: string;
+  eyebrow: string;
   link: LinkProps;
   image: { src: string; alt: string };
 };
 
 const NextChapter: FC<NextChapterProps> = ({ className, image, link }) => {
   return (
-    <BaseLink {...link} className={classNames('NextChapter', css.root, className)}>
+    <BaseLink {...link} className={classnames('NextChapter', css.root, className)}>
       <BaseImage {...image} className={css.image} />
 
       <div className={css.wrapper}>
         <div className={css.textWrapper}>
-          {/* <eyebrow> */}
+          {/* <Eyebrow  /> */}
           <div className={css.title}>{link.title}</div>
         </div>
+
+        <div className={classnames(css.circle, css.isWhite)}>{<div className={css.dot} />}</div>
       </div>
     </BaseLink>
   );
