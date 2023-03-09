a892718e6f98733c426773cef5201931609b6923
Jose Delgado <josdelgaur@gmail.com>
2023-03-09 18:14:31 -0300

    chore: tweak

--- src/components/DebugGrid/DebugGrid.tsx
+++ src/components/DebugGrid/DebugGrid.tsx
@@ -4,16 +4,21 @@ import classnames from 'classnames';
 import css from './DebugGrid.module.scss';
 import sassVars from '@/styles/export-vars.module.scss';
 
+import { useLayout } from '@/hooks';
+
 export type Props = {
   className?: string;
 };
 
 function DebugGrid({ className }: Props) {
+  const { layout } = useLayout();
+  const tablet = typeof window !== 'undefined' && layout.tablet;
+
   return (
     <div className={classnames(css.DebugGrid, className)}>
       <div className={css.container}>
         <div className={css.innerContainer}>
-          {Array(parseInt(sassVars.gridNumOfCols))
+          {Array(parseInt(!tablet ? sassVars.gridNumOfCols : sassVars.gridNumOfColsTablet))
             .fill(0)
             .map((_, index) => (
               <div className={css.column} key={index} />
