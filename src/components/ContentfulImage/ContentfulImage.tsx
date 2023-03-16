2128784adcd357dcb352b5695a8d3b87bab53e76
Luke Cochrane <luke.cochrane@jam3.com>
2023-03-16 10:46:26 -0400

    chore: adding hooks, storybook example

--- src/components/ContentfulImage/ContentfulImage.tsx
+++ src/components/ContentfulImage/ContentfulImage.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, ImgHTMLAttributes, memo, useEffect, useRef, useState } from 'react';
+import { forwardRef, ImgHTMLAttributes, memo, useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import classNames from 'classnames';
 
 import css from './ContentfulImage.module.scss';
@@ -45,10 +45,18 @@ const ContentfulImage = forwardRef<HTMLImageElement, ContentfulImageProps>(
     const isIntersection = useIntersectionObserver(rootRef.current);
     const isWebpSupported = useAppSelector((state) => state.isWebpSupported);
 
-    const imageSizeMobileValue = typeof imageSizeMobile === 'object' ? mobileColumns(imageSizeMobile) : imageSizeMobile;
-    const imageSizeTabletValue = typeof imageSizeTablet === 'object' ? tabletColumns(imageSizeTablet) : imageSizeTablet;
-    const imageSizeDesktopValue =
-      typeof imageSizeDesktop === 'object' ? desktopColumns(imageSizeDesktop) : imageSizeDesktop;
+    const imageSizeMobileValue = useMemo(
+      () => (typeof imageSizeMobile === 'object' ? mobileColumns(imageSizeMobile) : imageSizeMobile),
+      [imageSizeMobile]
+    );
+    const imageSizeTabletValue = useMemo(
+      () => (typeof imageSizeTablet === 'object' ? tabletColumns(imageSizeTablet) : imageSizeTablet),
+      [imageSizeTablet]
+    );
+    const imageSizeDesktopValue = useMemo(
+      () => (typeof imageSizeDesktop === 'object' ? desktopColumns(imageSizeDesktop) : imageSizeDesktop),
+      [imageSizeDesktop]
+    );
 
     useEffect(() => {
       if (loadImage) return;
@@ -57,24 +65,30 @@ const ContentfulImage = forwardRef<HTMLImageElement, ContentfulImageProps>(
       if (isIntersection) setLoadImage(true);
     }, [loadImage, withLazyLoad, isIntersection]);
 
-    const imageUrl = asset.fields.file.url.replace('//downloads', '//images');
-    const imageWidth = asset.fields.file.details.image.width;
+    const imageUrl = useMemo(() => asset.fields.file.url.replace('//downloads', '//images'), [asset.fields.file.url]);
+    const imageWidth = useMemo(() => asset.fields.file.details.image.width, [asset.fields.file.details.image]);
 
-    const buildSrc = (width: number, quality = imageQuality) => {
-      return `${imageUrl}?q=${quality}&w=${width}${isWebpSupported ? '&fm=webp' : ''}`;
-    };
+    const buildSrc = useCallback(
+      (width: number, quality = imageQuality) => {
+        return `${imageUrl}?q=${quality}&w=${width}${isWebpSupported ? '&fm=webp' : ''}`;
+      },
+      [imageQuality, imageUrl, isWebpSupported]
+    );
 
-    function buildSrcSet(q: number = imageQuality) {
-      let srcSetString = '';
-      const base = 320;
-      const hops = Math.floor(imageWidth / base);
-      const sizes = [...Array(hops)].map((_, i) => (i + 1) * base);
-      sizes.forEach((size) => {
-        srcSetString += `${buildSrc(size, q)} ${size}w,`;
-      });
+    const buildSrcSet = useCallback(
+      (q: number = imageQuality) => {
+        let srcSetString = '';
+        const base = 320;
+        const hops = Math.floor(imageWidth / base);
+        const sizes = [...Array(hops)].map((_, i) => (i + 1) * base);
+        sizes.forEach((size) => {
+          srcSetString += `${buildSrc(size, q)} ${size}w,`;
+        });
 
-      return srcSetString;
-    }
+        return srcSetString;
+      },
+      [buildSrc, imageQuality, imageWidth]
+    );
 
     if (!asset || !asset.fields || !asset.fields.file) return null;
 
@@ -87,7 +101,11 @@ const ContentfulImage = forwardRef<HTMLImageElement, ContentfulImageProps>(
         }
         srcSet={useSrcSet && loadImage ? buildSrcSet() : withLowResSwap ? buildSrcSet(15) : undefined}
         alt={asset.fields.description || asset.fields.title}
-        sizes={`(min-width: ${desktopWidth}) ${imageSizeDesktopValue}, (min-width: ${tabletWidth}) ${imageSizeTabletValue}, ${imageSizeMobileValue}`}
+        sizes={
+          useSrcSet
+            ? `(min-width: ${desktopWidth}) ${imageSizeDesktopValue}, (min-width: ${tabletWidth}) ${imageSizeTabletValue}, ${imageSizeMobileValue}`
+            : undefined
+        }
         ref={combinedRef}
         {...props}
       />
