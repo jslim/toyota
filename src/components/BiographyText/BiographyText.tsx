2e9866d5ad630bf8dc6063220e8e609b86982140
Raymond Chu <raymond.chu@jam3.com>
2023-04-06 12:32:57 -0400

    feature: add biography text component

--- src/components/BiographyText/BiographyText.tsx
+++ src/components/BiographyText/BiographyText.tsx
@@ -5,13 +5,27 @@ import css from './BiographyText.module.scss';
 
 export type BiographyTextProps = {
   className?: string;
+  titles: string[];
+  texts: string[];
 };
 
-const BiographyText: FC<BiographyTextProps> = ({ className }) => {
+const BiographyText: FC<BiographyTextProps> = ({ className, titles, texts }) => {
   return (
     <div className={classNames('BiographyText', css.root, className)}>
-      <div className={css.shortBio}></div>
-      <div className={css.longBio}></div>
+      <div className={css.titleContainer}>
+        {titles.map((title, key) => (
+          <h4 key={key} className={css.title}>
+            {title}
+          </h4>
+        ))}
+      </div>
+      <div className={css.textContainer}>
+        {texts.map((text, key) => (
+          <p key={key} className={css.text}>
+            {text}
+          </p>
+        ))}
+      </div>
     </div>
   );
 };
