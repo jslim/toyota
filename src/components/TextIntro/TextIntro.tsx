f221fd8b923266eac5c17c3692054c1b1554c439
Raymond Chu <raymond.chu@jam3.com>
2023-04-06 10:12:33 -0400

    chore: cleanup textintro css

--- src/components/TextIntro/TextIntro.tsx
+++ src/components/TextIntro/TextIntro.tsx
@@ -40,33 +40,31 @@ const TextIntro: FC<TextIntroProps> = ({ className, layout, eyebrow, header, des
   }, []);
 
   return (
-    <div>
-      <div
-        className={classNames(
-          'TextIntro',
-          css.root,
-          { [css.tabletColumnLayout]: layout === TextIntroLayout.HEADER_LEFT },
-          className
-        )}
-      >
-        {layout === TextIntroLayout.DEFAULT_BACKGROUND_IMAGE && (
-          <div className={css.backgroundImageContainer}>
-            <ToyotaBackground className={css.backgroundImage} />
+    <div
+      className={classNames(
+        'TextIntro',
+        css.root,
+        { [css.tabletColumnLayout]: layout === TextIntroLayout.HEADER_LEFT },
+        className
+      )}
+    >
+      {layout === TextIntroLayout.DEFAULT_BACKGROUND_IMAGE && (
+        <div className={css.backgroundImageContainer}>
+          <ToyotaBackground className={css.backgroundImage} />
+        </div>
+      )}
+      <div className={css.leftColumn}>
+        <Eyebrow text={eyebrow} />
+        {layout === TextIntroLayout.HEADER_LEFT && <h2 className={css.leftTitle}>{header}</h2>}
+      </div>
+      <div className={css.rightColumn} ref={contentRef}>
+        {layout !== TextIntroLayout.HEADER_LEFT && <h2 className={css.title}>{header}</h2>}
+        <p className={css.description}>{description}</p>
+        {link && ctaText && (
+          <div className={css.cta}>
+            <Cta title={ctaText} href={link} />
           </div>
         )}
-        <div className={css.leftColumn}>
-          <Eyebrow text={eyebrow} />
-          {layout === TextIntroLayout.HEADER_LEFT && <h2 className={css.leftTitle}>{header}</h2>}
-        </div>
-        <div className={css.rightColumn} ref={contentRef}>
-          {layout !== TextIntroLayout.HEADER_LEFT && <h2 className={css.title}>{header}</h2>}
-          <p className={css.description}>{description}</p>
-          {link && ctaText && (
-            <div className={css.cta}>
-              <Cta title={ctaText} href={link} />
-            </div>
-          )}
-        </div>
       </div>
     </div>
   );
