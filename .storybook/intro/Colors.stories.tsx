5e2538b59e8225922105a201d2a93c1fa8f98a5a
Raymond Chu <raymond.chu@jam3.com>
2023-04-11 11:48:00 -0400

    chore: add storybook to color

--- .storybook/intro/Colors.stories.tsx
+++ .storybook/intro/Colors.stories.tsx
@@ -3,11 +3,18 @@ import { Story } from '@storybook/react';
 
 import css from './Colors.module.scss';
 
+import { Color, getColorClass, getBackgroundColorClass } from '@/utils/colors.ts';
+
 import sass from '../../src/utils/sass';
 
 export default {
   title: 'intro/Colors'
 };
+
+interface colorsEnumProps {
+  backgroundColor: Color;
+  textColor: Color;
+}
 
 const Typographies: FC<{}> = () => {
   console.log(sass.color);
@@ -26,6 +33,40 @@ const Typographies: FC<{}> = () => {
   );
 };
 
+const Colors: FC<colorsEnumProps> = ({ backgroundColor, textColor }) => {
+  return (
+    <div
+      className={getBackgroundColorClass(backgroundColor)}
+      style={{ width: '100%', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+    >
+      <p className={getColorClass(textColor)}>
+        Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Vestibulum id ligula porta felis euismod
+        semper.
+      </p>
+    </div>
+  );
+};
+
 export const Default: Story<{}> = (args) => <Typographies {...args} />;
 
-Default.args = {};
+export const ColorsEnum = (args: colorsEnumProps) => <Colors {...args} />;
+
+ColorsEnum.args = {
+  backgroundColor: Color.DARK_GREY,
+  textColor: Color.WHITE
+};
+
+ColorsEnum.argTypes = {
+  backgroundColor: {
+    control: {
+      type: 'select',
+      options: Object.values(Color)
+    }
+  },
+  textColor: {
+    control: {
+      type: 'select',
+      options: Object.values(Color)
+    }
+  }
+};
