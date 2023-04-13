b53110726ab18496cd1958fc6fb0615842c1ae66
namniak <vnamnyak@gmail.com>
2023-04-13 14:14:06 -0400

    chore: removing explicit index page

--- src/pages/[lang]/[[...slug]].tsx
+++ src/pages/[lang]/[[...slug]].tsx
@@ -3,7 +3,13 @@ import { GetStaticProps } from 'next';
 import classNames from 'classnames';
 
 import { APIContentful } from '@/data/API';
-import { DefaultPageContentType, FilteredEntity, LocalizedPageParams, PageProps } from '@/data/types';
+import {
+  DefaultPageContentType,
+  FilteredEntity,
+  GenericEntity,
+  NestedLocalizedPageParams,
+  PageProps
+} from '@/data/types';
 
 import usePreviewData from '@/hooks/use-preview-data';
 import { getAllLangSlugs, getLocaleByLang } from '@/utils/locales';
@@ -14,6 +20,8 @@ type DefaultPageData = FilteredEntity<DefaultPageContentType>;
 export interface DefaultPageProps extends PageProps {
   data: DefaultPageData;
 }
+
+const homepageSlug = 'homepage';
 
 const DefaultPage: FC<DefaultPageProps> = ({ data }) => {
   const pageData = usePreviewData({
@@ -30,7 +38,27 @@ const DefaultPage: FC<DefaultPageProps> = ({ data }) => {
 };
 
 export async function getStaticPaths() {
-  const paths = getAllLangSlugs();
+  const accessToken = process.env.CONTENTFUL_DELIVERY_API_ACCESS_TOKEN; // IMPORTANT: keep content token within 'getStaticProps' of each page
+  const spaceId = process.env.CONTENTFUL_SPACE_ID; // IMPORTANT: keep space ID within 'getStaticProps' of each page
+
+  const apiContentful = new APIContentful({ spaceId, accessToken });
+  const data = await apiContentful.getEntriesByContentType('defaultPage', {});
+  const pageSlugs = data.items.map((entry: GenericEntity) => entry.fields!.slug);
+  const langPaths = getAllLangSlugs();
+
+  const paths: Array<{ [key: string]: NestedLocalizedPageParams }> = [];
+
+  pageSlugs.forEach((slug: string) => {
+    paths.push(
+      ...langPaths.map(({ params }) => ({
+        params: {
+          lang: params.lang,
+          slug: slug === homepageSlug ? [] : [slug]
+        }
+      }))
+    );
+  });
+
   return {
     paths,
     fallback: false
@@ -38,14 +66,14 @@ export async function getStaticPaths() {
 }
 
 export const getStaticProps: GetStaticProps<DefaultPageProps> = async ({ params }) => {
-  const { lang } = params as LocalizedPageParams;
+  const { lang, slug } = params as NestedLocalizedPageParams;
   const locale = getLocaleByLang(lang);
 
   const accessToken = process.env.CONTENTFUL_DELIVERY_API_ACCESS_TOKEN; // IMPORTANT: keep content token within 'getStaticProps' of each page
   const spaceId = process.env.CONTENTFUL_SPACE_ID; // IMPORTANT: keep space ID within 'getStaticProps' of each page
 
   const apiContentful = new APIContentful({ spaceId, accessToken });
-  const data = await apiContentful.getEntryBySlug('homepage', 'defaultPage', { locale, include: 10 });
+  const data = await apiContentful.getEntryBySlug(slug?.[0] ?? homepageSlug, 'defaultPage', { locale, include: 10 });
 
   return {
     props: {
