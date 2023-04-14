699f6e82b9c6bc61f0fb20c40fe91c7f486777ff
Raymond Chu <raymond.chu@jam3.com>
2023-04-14 17:48:15 -0400

    chore: default background color in sectionwrapper

--- src/components/SectionWrapper/SectionWrapper.tsx
+++ src/components/SectionWrapper/SectionWrapper.tsx
@@ -24,7 +24,7 @@ const SectionWrapper: FC<SectionWrapperProps> = ({
   title,
   children,
   theme = variants.DARK,
-  backgroundColor = (variants.DARK && Color.DARK_GREY) || Color.WHITE
+  backgroundColor = Color.DARK_GREY
 }) => {
   return (
     <div
