0970e7251af6d840f1725435fafbb3578381772e
Raymond Chu <raymond.chu@jam3.com>
2023-04-20 09:33:03 -0400

    chore: add video option to embed asset rich text

--- src/utils/parsers/rich-text-parser.tsx
+++ src/utils/parsers/rich-text-parser.tsx
@@ -1,12 +1,14 @@
 import { ReactNode } from 'react';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import { BLOCKS, Document, INLINES } from '@contentful/rich-text-types';
+import noop from 'no-op';
 
 import { FilteredEntity } from '@/data/types';
 import { ContentfulImageAsset } from '@/data/types';
 
 import BaseLink, { Target } from '@/components/BaseLink/BaseLink';
 import ContentfulImage from '@/components/ContentfulImage/ContentfulImage';
+import VideoPlayer from '@/components/VideoPlayer/VideoPlayer';
 
 import { getPageBlocks } from './get-page-blocks';
 
@@ -45,8 +47,40 @@ export const parseContentfulRichText = (richText: Document, className?: string):
       },
 
       [BLOCKS.EMBEDDED_ASSET]: (node: ContentfulImageNodeType) => {
-        const imageAsset = node.data.target;
-        return imageAsset && <ContentfulImage className={className} asset={imageAsset} />;
+        const contentType = node?.data?.target?.fields.file.contentType;
+
+        if (contentType?.includes('image')) {
+          const imageAsset = node.data.target;
+          return imageAsset && <ContentfulImage className={className} asset={imageAsset} />;
+        }
+
+        if (contentType?.includes('video')) {
+          const videoSrc = node?.data?.target?.fields.file.url;
+          return (
+            videoSrc && (
+              <VideoPlayer
+                src={videoSrc}
+                style={{ width: '720px', height: '306px' }}
+                poster={''}
+                windowWidth={window.innerWidth}
+                windowHeight={window.innerHeight}
+                togglePlayOnClick={true}
+                startTime={0}
+                allowKeyboardControl={true}
+                showControlsOnLoad={true}
+                hasControls={true}
+                hasPlayOnly={false}
+                autoPlayDelay={0}
+                disableBackgroundCover={true}
+                controlsTimeout={2.5}
+                togglePlaying={noop}
+                onEnd={noop}
+              />
+            )
+          );
+        }
+
+        return null;
       }
     }
   };
