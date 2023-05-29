e46a2bcdc2ba053d9b824ccd671a5da3c69c1dde
DariaV13 <daria.vyskubina@jam3.com>
2023-05-29 17:47:55 -0400

    fix: add space to non-hero pages

--- src/components/PageNotFound/PageNotFound.tsx
+++ src/components/PageNotFound/PageNotFound.tsx
@@ -5,7 +5,10 @@ import css from './PageNotFound.module.scss';
 
 import { PageProps } from '@/data/types';
 
+import SectionWrapper from '@/components/SectionWrapper/SectionWrapper';
 import TextIntro, { TextIntroLayout } from '@/components/TextIntro/TextIntro';
+
+import { Color } from '@/utils/colors';
 
 import { useAppSelector } from '@/redux';
 
@@ -20,13 +23,16 @@ const PageNotFound: FC<PageNotFoundProps> = ({ className }) => {
 
   return (
     <main className={classNames('PageNotFound', css.root, className)}>
-      <TextIntro
-        layout={TextIntroLayout.DEFAULT}
-        eyebrow={'404'}
-        header={notFoundPageHeader}
-        description={notFoundPageDescription}
-        ctaProps={{ title: notFoundPageButton, href: '/' }}
-      />
+      <SectionWrapper backgroundColor={Color.WHITE}>
+        <TextIntro
+          layout={TextIntroLayout.DEFAULT}
+          eyebrow={'404'}
+          header={notFoundPageHeader}
+          description={notFoundPageDescription}
+          ctaProps={{ title: notFoundPageButton, href: '/' }}
+          className={css.text404}
+        />
+      </SectionWrapper>
     </main>
   );
 };
